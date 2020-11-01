893a0ff648f4bb9ba9cad6cd8935959c08324457
Ricardo Cruz <ricardo.pdm.cruz@gmail.com>
2020-11-01 13:05:56 +0000

    Changed portfolio positions. Hide contact elements for mobile.

--- aux/cv/gencv.py
+++ aux/cv/gencv.py
@@ -2,6 +2,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('yaml')
 parser.add_argument('template')
+parser.add_argument('output')
 args = parser.parse_args()
 
 from jinja2 import Template
@@ -9,7 +10,4 @@ import yaml
 cv = yaml.load(open(args.yaml), Loader=yaml.Loader)
 template = Template(open(args.template).read(), trim_blocks=True, lstrip_blocks=True)
 out = template.render(**cv)
-
-outname = args.yaml.split('.')[0] + '.' + args.template.split('.')[-1]
-open(outname, 'w').write(out)
-
+open(args.output, 'w').write(out)
