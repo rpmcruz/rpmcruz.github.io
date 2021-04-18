8435968d64457ab9d00df4594fb9776570a73e65
Ricardo Cruz <ricardo.pdm.cruz@gmail.com>
2021-04-18 18:19:15 +0100

    Re-worked CV design.

--- aux/cv/gencv.py
+++ aux/cv/gencv.py
@@ -5,9 +5,17 @@ parser.add_argument('template')
 parser.add_argument('output')
 args = parser.parse_args()
 
+import re
+def myurlize(text, start_block, end_block):
+    return re.sub(r'(http\S+)', start_block + r'\1' + end_block, text)
+
 from jinja2 import Template
 import yaml
 cv = yaml.load(open(args.yaml), Loader=yaml.Loader)
-template = Template(open(args.template).read(), trim_blocks=True, lstrip_blocks=True)
-out = template.render(**cv)
+template = Template(open(args.template).read(),
+    trim_blocks=True, lstrip_blocks=True,
+    block_start_string='@%', block_end_string='%@',
+    variable_start_string='@@', variable_end_string='@@',
+    comment_start_string='@#', comment_end_string='#@')
+out = template.render(cv=cv, myurlize=myurlize)
 open(args.output, 'w').write(out)
