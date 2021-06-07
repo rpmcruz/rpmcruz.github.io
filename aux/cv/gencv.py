041e66184f7f41149f618bd44da399087f079209
Ricardo Cruz <ricardo.pdm.cruz@gmail.com>
2021-06-07 17:11:12 +0100

    Major re-work of CV templates to make them a little more professional, especially the PDF version.

--- aux/cv/gencv.py
+++ aux/cv/gencv.py
@@ -5,10 +5,6 @@ parser.add_argument('template')
 parser.add_argument('output')
 args = parser.parse_args()
 
-import re
-def myurlize(text, start_block, end_block):
-    return re.sub(r'(http\S+)', start_block + r'\1' + end_block, text)
-
 from jinja2 import Template
 import yaml
 cv = yaml.load(open(args.yaml), Loader=yaml.Loader)
@@ -17,5 +13,5 @@ template = Template(open(args.template).read(),
     block_start_string='@%', block_end_string='%@',
     variable_start_string='@@', variable_end_string='@@',
     comment_start_string='@#', comment_end_string='#@')
-out = template.render(cv=cv, myurlize=myurlize)
+out = template.render(cv=cv)
 open(args.output, 'w').write(out)
